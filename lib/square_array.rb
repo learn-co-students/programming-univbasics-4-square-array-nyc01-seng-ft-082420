02af58a91b89c4fe3a3b81e87e3f48ec6a2ab12b
Ian Rosen <irosen419@gmail.com>
2020-04-04 01:52:58 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
+require 'pry'
+
 def square_array(array)
-  # your code here
+  squared = []
+  counter = 0
+  
+  while array[counter] do
+    squared.push(array[counter] ** 2)
+    counter += 1
+  end
+  return squared
 end
