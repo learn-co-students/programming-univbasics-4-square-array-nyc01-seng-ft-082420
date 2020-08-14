5af6de9488c695b7e57d2bfd49752b3f1c54ba8b
C. Chang <chloechang416@gmail.com>
2020-08-14 01:43:39 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,29 @@
+#
+# array = [2, 4, 6, 8]
+# new_numbers = []
+#
+# array.length.times do |index|
+#     new_numbers.push(array[index] ** 2)
+# end
+# new_numbers
+#
+#
+#
+
+
+# def square_array(array)
+#   count = 0
+#   while count < numbers.length do
+#     numbers.index ** 2
+#   end
+# end
+
 def square_array(array)
-  # your code here
+  squared = []
+  count = 0
+  while count < array.size do
+    squared << (array[count] ** 2)
+    count = count + 1
+  end
+  squared
 end
